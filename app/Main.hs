704d1fc0a9aab302fec93d40b3987d593e00070c
VetoPlayer <andrewlloys@gmail.com>
2017-12-24 13:59:05 +0100

    Parser implementation

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,65 @@
 module Main where
 
 import Lib
+import Data.Char
+
+
+-- Operators
+
+data Operator = Plus | Minus | Times | Div
+    deriving (Show, Eq)
+
+opToChar :: Operator -> Char
+opToChar Plus = '+'
+opToChar Minus = '-'
+opToChar Times = '*'
+opToChar Div = ':'
+
+operator:: Char -> Operator
+operator c  | c == '+' = Plus
+            | c == '-' = Minus
+            | c == '*' = Times
+            | c == '/' = Div
+
+
+data Token = TokOp Operator
+            | TokIdent String
+            | TokNum Int
+            | TokSpace
+    deriving (Show, Eq)
+
+showContent:: Token -> String
+showContent (TokOp op) = opToChar op  
+showContent (TokIdent id) = id 
+showContent (TokNum num) = show num
+
+
+tokenizeChar :: Char -> Token
+tokenizeChar x
+        | elem x "+-*/" = TokOp (operator x)
+        | isDigit x = TokNum (digitToInt x)
+        | isAlpha x = TokIdent [c] 
+        | isSpace x = TokSpace
+        | otherwise = error $ "Cannot Tokenize" ++ [x]
+
+
+
+
+data Expression
+
+parse:: [Token] -> Expression
+parse = undefined
+
+
+evaluate:: Expression -> Double
+evaluate = undefined
+
+
 
 main :: IO ()
 main = do
-    sayhi "wei"
-    sayhi "bye"
+    line <- getLine
+    putStrLn line
+    main
 
-sayhi str = do
-    putStrLn str
-    putChar '\n'
+
